33062e6daed7bc45d55728a856411076a13dc87d
Eric Wittmann <eric.wittmann@gmail.com>
2019-04-01 11:51:32 -0400

    added a release script for apicurito

---
+++ release-apicurito.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+set -e
+
+echo "---------------------------------------------------"
+echo " Releasing Apicurito.  Many steps to follow."
+echo " Please play along at home..."
+echo "---------------------------------------------------"
+echo ""
+echo ""
+
+
+BRANCH=$1
+RELEASE_VERSION=$2
+
+if [ -f .release.env ]
+then
+  source ./.release.env
+else
+  echo "Missing file: .release.env.  Please create this file and add the following env variables:"
+  echo "---"
+  echo "GITHUB_AUTH_PAT=<your_GitHub_PAT>"
+  echo "GPG_PASSPHRASE=<your_GPG_passphrase>"
+  echo "---"
+  echo ""
+  exit 1
+fi
+
+if [ "x$GITHUB_AUTH_PAT" = "x" ]
+then
+  echo "Environment variable missing from .release.env file: GITHUB_AUTH_PAT"
+fi
+if [ "x$GPG_PASSPHRASE" = "x" ]
+then
+  echo "Environment variable missing from .release.env file: GPG_PASSPHRASE"
+fi
+
+echo "---------------------------------------------------"
+echo " Tell me what version and branch we're releasing!"
+echo "---------------------------------------------------"
+echo ""
+
+if [ "x$RELEASE_VERSION" = "x" ]
+then
+  read -p "Release Version: " RELEASE_VERSION
+fi
+
+if [ "x$BRANCH" = "x" ]
+then
+  read -p "Release Branch: [master] " BRANCH
+fi
+if [ "x$BRANCH" = "x" ]
+then
+  BRANCH=master
+fi
+
+
+echo "######################################"
+echo "Release Version:  $RELEASE_VERSION"
+echo "Branch:           $BRANCH"
+echo "######################################"
+echo ""
+
+
+echo "---------------------------------------------------"
+echo " Resetting 'target' directory."
+echo "---------------------------------------------------"
+echo ""
+rm -rf target
+mkdir -p target
+cp README.md target/README.md
+gpg -s target/README.md
+rm target/README.md.gpg
+
+
+echo "---------------------------------------------------"
+echo " Checking out required git repos."
+echo "---------------------------------------------------"
+echo ""
+mkdir -p target/git-repos
+cd target/git-repos
+git clone git@github.com:apicurio/apicurito.git
+
+
+echo "---------------------------------------------------"
+echo " Update version #s and validate builds"
+echo "---------------------------------------------------"
+rm -rf ~/.m2/repository/io/apicurio
+pushd .
+cd apicurito
+git checkout $BRANCH
+mvn versions:set -DnewVersion=$RELEASE_VERSION
+find . -name '*.versionsBackup' -exec rm -f {} \;
+sed -i "s/version.:.*/version\": \"$RELEASE_VERSION\",/g" ui/package.json
+mvn clean install -D::image
+git add ui/package.json pom.xml ui/pom.xml
+git commit -m "Prepare for release $RELEASE_VERSION"
+git push origin $BRANCH -s -S
+git tag -a -s -m "Tagging release $RELEASE_VERSION" $RELEASE_VERSION
+git push origin $RELEASE_VERSION -s -S
+
+
+echo "---------------------------------------------------"
+echo "Pushing docker images."
+echo "---------------------------------------------------"
+docker push apicurio/apicurito-ui:latest
+docker tag apicurio/apicurito-ui apicurio/apicurito-ui:$RELEASE_VERSION
+docker push apicurio/apicurito-ui:$RELEASE_VERSION
+
+echo ""
+echo ""
+echo "---------------------------------------------------"
+echo " ALL DONE!"
+echo "---------------------------------------------------"
+
